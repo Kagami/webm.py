cfea3b23dc00d28ba16ab05c6e6444f14d1cb3ba
Kagami Hiiragi <kagami@genshiken.org>
2015-04-21 19:21:19 +0300

    Cosmetics

--- upload.py
+++ upload.py
@@ -16,11 +16,14 @@ pandoc.core.PANDOC_PATH = 'pandoc'
 
 def main():
     cmd = 'python setup.py sdist'
-    if '-u' in sys.argv:
+    # NOTE: On Py3 both of this are unicode strings, on Py2 both are
+    # bytes. We can't use unicode_literals in Py2 because sys.argv may
+    # contain non-ascii encoded string and python will complain.
+    if '-u' in sys.argv[1:]:
         cmd += ' upload'
     doc = pandoc.Document()
-    # NOTE: pyandoc accepts bytes and return bytes. It doesn't work with
-    # Py3 though.
+    # NOTE: pyandoc accepts bytes and returns bytes. Also it doesn't
+    # work with Py3.
     doc.markdown = open('README.md', 'rb').read()
     f = open('README.rst', 'wb')
     f.write(doc.rst)
