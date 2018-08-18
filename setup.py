73b6b0ce75107e77dad2987cdf3a624c7a7530cc
Kagami Hiiragi <kagami@genshiken.org>
2018-08-18 19:44:27 +0300

    Modernize setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except Exception:
 
 
 setup(
-    # Reference: <http://pythonhosted.org/distribute/setuptools.html>.
+    # Reference: <https://setuptools.readthedocs.io/en/latest/setuptools.html>.
     name='webm',
     version=__version__,
     author='Kagami Hiiragi',
@@ -35,6 +35,9 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
         'Operating System :: OS Independent',
         'Development Status :: 4 - Beta',
